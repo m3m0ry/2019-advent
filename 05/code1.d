68c921cac8d831039a7d17d1f3b3396667dc735e
Jan Hönig <jan.hoenig@fau.de>
2019-12-05 20:19:51 +0100

    Done day 5

--- 05/code1.d
+++ 05/code1.d
@@ -7,20 +7,44 @@ import std.string;
 import std.exception;
 
 
-function(int)[int] ops;
+//TODO do some smarter dispatch. see here: http://arsdnet.net/dcode/book/chapter_09/07/
+
+
+Program function(Program)[int] ops;
+
+static this()
+{
+  ops[1] = &opArithmetic!"+";
+  ops[2] = &opArithmetic!"*";
+  ops[3] = &opRead;
+  ops[4] = &opWrite;
+  ops[5] = &opJump!"!=";
+  ops[6] = &opJump!"==";
+  ops[7] = &opArithmetic!"<";
+  ops[8] = &opArithmetic!"==";
+  ops[99] = &opStop;
+}
 
 struct Program
 {
   int ip = 0;
   int[] program;
 
+  int op()
+  {
+    return program[ip];
+  }
 
   ref int opIndex(size_t i, int mode)
   {
-    if (mode == 0)
+    if (mode == 1)
+    {
       return program[i];
-    else if (mode == 1)
+    }
+    else if (mode == 0)
+    {
       return program[program[i]];
+    }
     throw new Exception("Invalid mode");
   }
 
@@ -29,38 +53,66 @@ struct Program
   @property int opDollar(size_t dim : 0)() { return program.length;}
 }
 
-Program operation(string op)(Program p)
+int mode(int op, int i)
 {
-    p[p.ip+3, 1] = mixin(`p[p.ip+2, 1]` ~ op ~ `p[p.ip+1, 1]`);
+  return op.format!"%05d"[i..i+1].to!int;
+}
+
+Program opArithmetic(string op)(Program p)
+{
+    p[p.ip+3, mode(p.op, 0)] = mixin(`p[p.ip+1, mode(p.op, 2)]` ~ op ~ `p[p.ip+2, mode(p.op, 1)]`);
+    p.ip += 4;
     return p;
 }
 
-Program compute(Program program, int ipIncr = 0)
+Program opRead(Program p)
 {
-  program.ip += ipIncr;
-  int instruction = program[program.ip];
-  if (instruction == 1)
-  {
-    return compute(operation!"+"(program), 4);
-  }
-  else if (instruction == 2)
-  {
-    return compute(operation!"*"(program), 4);
-  }
-  else if (instruction == 99)
+  write("Input number:");
+  p[p.ip+1, mode(p.op, 2)] = readln.strip.to!int;
+  p.ip += 2;
+  return p;
+}
+
+Program opWrite(Program p)
+{
+  writeln(p[p.ip+1, mode(p.op, 2)]);
+  p.ip += 2;
+  return p;
+}
+
+Program opJump(string op)(Program p)
+{
+  if (mixin(`p[p.ip+1, mode(p.op, 2)]`~(op)~`0`))
+    p.ip = p[p.ip+2, mode(p.op, 1)];
+  else
+    p.ip += 3;
+  return p;
+}
+
+Program opStop(Program p)
+{
+  return p;
+}
+
+Program compute(Program program)
+{
+  int instruction = program.op.format!"%05d"[$-2 .. $].to!int;
+  program = ops[instruction](program);
+
+  if (instruction == 99)
   {
     return program;
   }
   else
   {
-    throw new Exception("Invalid instruction");
+    return compute(program);
   }
 }
 
 void main()
 {
-  int[] code = readText("input02.txt").strip.split(",").map!(a => to!int(a)).array;
-  //code = "1,0,0,0,99".strip.split(",").map!(a=>to!int(a)).array;
+  int[] code = readText("input01.txt").strip.split(",").map!(a => to!int(a)).array;
+  //code = "3,9,7,9,10,9,4,9,99,-1,8".strip.split(",").map!(a=>to!int(a)).array;
   auto program = Program(0, code);
   program = compute(program);
 
