372e4fb7a3a1cf67a19deca5fdce639973d7d9b9
Jan Hönig <jan.hoenig@fau.de>
2019-12-12 20:17:35 +0100

    Day 12 part 2 still not done

--- 12/code2.d
+++ 12/code2.d
@@ -3,13 +3,31 @@ import std.conv;
 import std.format;
 import std.algorithm;
 import std.array;
+import std.range;
 import std.math : abs, sgn;
 import std.format;
+import std.numeric;
+
+int lcm(int a, int b)
+{
+  return abs((a*b)/gcd(a,b));
+}
 
 struct Moon
 {
   int[3] pos;
   int[3] vel;
+  int[3] periods;
+
+  bool allPeriods()
+  {
+    return all!"a != 0"(periods.dup);
+  }
+
+  bool isPeriod(Moon o, int i)
+  {
+    return o.pos[i] == pos[i] && vel[i] == o.vel[i];
+  }
 
   int potential()
   {
@@ -38,7 +56,7 @@ struct Moon
       vel[i] -= o.pos[i] < pos[i];
       vel[i] += o.pos[i] > pos[i];
     }
- }
+  }
 
   string toString()
   {
@@ -54,14 +72,15 @@ void main()
   foreach (line; f.byLine)
   {
     int x, y, z;
-    line.formattedRead!"<x=%d, y=%d, z=%d"(x, y, z);
+    line.formattedRead!"<x=%d, y=%d, z=%d>"(x, y, z);
     moons ~= Moon([x, y, z]);
   }
-  immutable Moon[] init = moons.idup;
 
-  int i;
+  Moon[] init = moons.dup;
+  int step = 0;
   while(true)
   {
+    step++;
     foreach (ref moon; moons)
     {
       foreach (o; moons)
@@ -69,18 +88,28 @@ void main()
         moon.gravity(o);
       }
     }
-    foreach (ref moon; moons)
+    foreach (i, ref moon; moons)
     {
       moon.applyVelocity;
+      foreach(j; 0..3)
+      {
+        if(!moon.periods[j] && moon.isPeriod(init[i], j.to!int))
+        {
+          moon.periods[j] = step;
+        }
+      }
     }
-    if (moons == init)
-      break;
-    else
+    if (all!(a => a.allPeriods)(moons))
     {
-      i++;
+      break;
     }
-    if(i%100_000 == 0)
-      writeln(i);
   }
-  writeln(i);
+  int[] moonPerios;
+  foreach (moon; moons)
+  {
+    writeln(moon.periods);
+    moonPerios ~= moon.periods.fold!((a,b) => lcm(a,b));
+  }
+  writeln(moonPerios);
+  writeln(moonPerios.fold!((a,b) => lcm(a,b)));
 }
