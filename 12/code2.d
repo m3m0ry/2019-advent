528961aadec511e6c712adc1726f1471ac09b741
Jan Hönig <jan.hoenig@fau.de>
2019-12-12 15:00:53 +0100

    Day 12 working

---
+++ 12/code2.d
@@ -0,0 +1,86 @@
+import std.stdio;
+import std.conv;
+import std.format;
+import std.algorithm;
+import std.array;
+import std.math : abs, sgn;
+import std.format;
+
+struct Moon
+{
+  int[3] pos;
+  int[3] vel;
+
+  int potential()
+  {
+    return pos.fold!((a, b) => abs(a) + abs(b));
+  }
+
+  int kinetic()
+  {
+    return vel.fold!((a, b) => abs(a) + abs(b));
+  }
+
+  int total()
+  {
+    return potential * kinetic;
+  }
+
+  void applyVelocity()
+  {
+    pos[] += vel[];
+  }
+
+  void gravity(Moon o)
+  {
+    foreach (i; 0 .. pos.length)
+    {
+      vel[i] -= o.pos[i] < pos[i];
+      vel[i] += o.pos[i] > pos[i];
+    }
+ }
+
+  string toString()
+  {
+    return format!"pos=<x=%d, y=%d, z=%d>, vel=<x=%d, y=%d, z=%d>"(pos[0],
+        pos[1], pos[2], vel[0], vel[1], vel[2]);
+  }
+}
+
+void main()
+{
+  auto f = File("input01.txt");
+  Moon[] moons;
+  foreach (line; f.byLine)
+  {
+    int x, y, z;
+    line.formattedRead!"<x=%d, y=%d, z=%d"(x, y, z);
+    moons ~= Moon([x, y, z]);
+  }
+  immutable Moon[] init = moons.idup;
+
+  int i;
+  while(true)
+  {
+    foreach (ref moon; moons)
+    {
+      foreach (o; moons)
+      {
+        moon.gravity(o);
+      }
+    }
+    foreach (ref moon; moons)
+    {
+      moon.applyVelocity;
+    }
+    if (moons == init)
+      break;
+    else
+    {
+      i++;
+    }
+    if(i%100_000 == 0)
+      writeln(i);
+  }
+  writeln(i);
+}
