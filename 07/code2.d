efc7b57cc0ae897275bb1be87dee8872eb609e0b
Jan Hönig <jan.hoenig@fau.de>
2019-12-22 21:20:57 +0100

    Done day 07. Holy hell this is uggly

--- 07/code2.d
+++ 07/code2.d
@@ -127,20 +127,25 @@ void main()
 {
   int[] code = readText("input01.txt").strip.split(",").map!(a => to!int(a)).array;
   code = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5".strip.split(",").map!(a=>to!int(a)).array;
+  //code = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0".strip.split(",").map!(a=>to!int(a)).array;
+  //code = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10".strip.split(",").map!(a=>to!int(a)).array;
   auto program = Program(0, code);
   int best;
   foreach(permutation; iota(5, 10).permutations)
   {
-    if(permutation.array != [9,8,7,6,5])
-      continue;
+    //if(permutation.array != [9,8,7,6,5])//[9,7,8,5,6])
+    //  continue;
     Program[5] programs;
     programs[0 .. $] = program;
     auto first = true;
+    ioStack.clear;
     ioStack.insertFront(0);
+    int runs;
     while(programs[$-1].state != Status.Stop)
     {
       foreach(i, p; permutation.enumerate)
       {
+        writeln("Phase: ", i);
         foreach(e; ioStack)
           writeln("IO: ", e);
         if (first)
@@ -148,10 +153,11 @@ void main()
         programs[i] = compute(programs[i]);
       }
       first = false;
+      programs.each!(a => writeln(a.state));
     }
     best = max(best, ioStack.front);
-    ioStack.removeFront;
+    if(best == ioStack.front)
+      writeln(permutation);
   }
   writeln(best);
-  writeln(ioStack.empty);
 }
